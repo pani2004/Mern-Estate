1103c7bbd6fefb38f23f51c9948696a65548038b
pani2004 <debashispani61@gmail.com>
2024-07-15 04:17:01 +0530

    installation

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./db/index.js"
-import app from "./app.js"
+import path from "path"
+import express from "express"
+import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.routes.js"
+import listingRouter from "./routes/listing.routes.js"
+import cookieParser from "cookie-parser"
+const app = express()
 dotenv.config({
     path:'./.env'
 })
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
+app.use("/api/listing",listingRouter)
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 connectDb().then(()=>{
     app.listen(3000,()=>{
         console.log("Server is running on port 3000")
