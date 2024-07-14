2cf4533b70ec9af3cad2dfeac59a264e291bd114
pani2004 <debashispani61@gmail.com>
2024-07-15 03:46:18 +0530

    Completed the search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { CiSearch } from "react-icons/ci";
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Header() {
+  const [searchTerm,setSearchTerm] = useState("")
+  const navigate = useNavigate();
   const { currentUser } = useSelector(state => state.user);
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,9 +30,13 @@ function Header() {
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <CiSearch />
+        <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type="text" placeholder='Search...'  
+          className='bg-transparent focus:outline-none w-24 sm:w-64'
+          value={searchTerm} 
+          onChange={(e)=>setSearchTerm(e.target.value)}/>
+          <button><CiSearch className="text-slate-600"/></button>
+          
         </form>
         <ul className='flex gap-4'>
           <Link to='/' className='hidden sm:inline text-slate-700 hover:scale-105'><li>Home</li></Link>
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -82,12 +82,19 @@ function Listing() {
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
           <p className='text-2xl font-semibold'>
-              {listing.data.name} - ${' '}
-              {listing.data.offer
-                ? listing.data.discountPrice.toLocaleString('en-US')
-                : listing.data.regularPrice.toLocaleString('en-US')}
-              {listing.data.type === 'rent' && ' / month'}
-            </p>
+            {listing.data.name} - {' '}
+            {listing.data.offer && (
+            <>
+            <span className="text-gray-400 line-through">${listing.data.regularPrice.toLocaleString('en-US')}</span>{' '}
+            ${listing.data.discountPrice.toLocaleString('en-US')}
+           </>
+           )}
+            {!listing.data.offer && (
+            `$${listing.data.regularPrice.toLocaleString('en-US')}`
+        )}
+            {listing.data.type === 'rent' && ' / month'}
+           </p>
+
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
               <FaMapMarkerAlt className='text-green-700' />
               {listing.data.address}
@@ -99,7 +106,7 @@ function Listing() {
                 {
                     listing.data.offer && (
                         <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                        ${+listing.data.regularPrice - +listing.data.discountPrice}
+                        ${+listing.data.regularPrice - +listing.data.discountPrice} Off
                         </p>
                     )
                 }
